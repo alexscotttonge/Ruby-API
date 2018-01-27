afb48ce6b197c80eae7aaae03071289e295a29fe
Alex <alex@scotttonge.com>
2018-01-27 15:57:15 +0000

    spiked code, method broken, need to change API gem to rest-client

--- lib/app.rb
+++ lib/app.rb
@@ -1,4 +1,5 @@
 require 'net/http'
+require 'json'
 
 class App
 
@@ -6,6 +7,19 @@ class App
     uri = URI('https://driftrock-dev-test-2.herokuapp.com')
     response = Net::HTTP.get_response(uri)
     response.code
+  end
+
+  def all_purchases
+    # total purchases = 40001
+    per_page = 1
+    page = 1
+    url_extension = "?per_page=#{per_page}&page=#{page}"
+    uri = URI('https://driftrock-dev-test-2.herokuapp.com/purchases' + url_extension )
+    response = Net::HTTP.get_response(uri)
+    data = JSON.parse(response)
+    number_of_entries = data['data']
+
+    number_of_entries.each_with_object(Hash.new(0)) { |order, hash| hash[number_of_entries['spend']] += 1 }.max_by { |k, v| v }
   end
 
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,8 +1,25 @@
 require 'spec_helper'
 
 describe App do
-  it 'can connect to the API' do
-    client = App.new
-    expect(client.api_request).to eq '200'
+
+  describe '#api_request' do
+    xit 'can connect to the API' do
+      client = App.new
+      expect(client.api_request).to eq '200'
+    end
   end
+
+  describe '#most_sold' do
+    xit 'returns the most sold item' do
+      client = App.new
+
+    end
+  end
+
+
 end
+
+
+# change net/http to rest client and look in trash for correct setup
+# change initial test
+# Add simple cov gem
