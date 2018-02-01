5749510b8c35f95725499459df7c64c442060a2a
Alex <alex@scotttonge.com>
2018-02-01 14:22:23 +0000

    changed net/http to rest-client

--- lib/app.rb
+++ lib/app.rb
@@ -1,25 +1,12 @@
-require 'net/http'
+require 'rest-client'
 require 'json'
 
 class App
 
   def api_request
-    uri = URI('https://driftrock-dev-test-2.herokuapp.com')
-    response = Net::HTTP.get_response(uri)
+    url = 'https://driftrock-dev-test-2.herokuapp.com'
+    response = RestClient.get(url)
     response.code
-  end
-
-  def all_purchases
-    # total purchases = 40001
-    per_page = 1
-    page = 1
-    url_extension = "?per_page=#{per_page}&page=#{page}"
-    uri = URI('https://driftrock-dev-test-2.herokuapp.com/purchases' + url_extension )
-    response = Net::HTTP.get_response(uri)
-    data = JSON.parse(response)
-    number_of_entries = data['data']
-
-    number_of_entries.each_with_object(Hash.new(0)) { |order, hash| hash[number_of_entries['spend']] += 1 }.max_by { |k, v| v }
   end
 
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -3,23 +3,12 @@ require 'spec_helper'
 describe App do
 
   describe '#api_request' do
-    xit 'can connect to the API' do
+    it 'can connect to the API' do
       client = App.new
-      expect(client.api_request).to eq '200'
+      expect(client.api_request).to eq 200
     end
   end
-
-  describe '#most_sold' do
-    xit 'returns the most sold item' do
-      client = App.new
-
-    end
-  end
-
 
 end
 
-
-# change net/http to rest client and look in trash for correct setup
-# change initial test
 # Add simple cov gem
