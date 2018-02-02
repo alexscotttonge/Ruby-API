18ba05275eea6ebb3646b79947db8f64f34778af
Alex <alex@scotttonge.com>
2018-02-02 15:17:25 +0000

    returns email address of most loyal user

--- lib/highest_value.rb
+++ lib/highest_value.rb
@@ -11,17 +11,38 @@ class MostLoyal
     number_of_entries.each_with_object(Hash.new(0)) { |order, hash| hash[order['user_id']] += 1 }.max_by { |k, v| v }
   end
 
+  def print_users
+    purchase_id = "NW0H-Q6RH-5L31-4ZHU"
+    number_of_entries = parse_users['data']
+    number_of_entries.each do |user|
+      if user['id'] == purchase_id
+      puts user['email']
+      end
+    end
+  end
+
   private
 
   def parse_most_loyal
-    url = most_loyal_extension
+    url = purchase_url
     response = RestClient.get(url)
     data = JSON.parse(response)
   end
 
-  def most_loyal_extension
+  def parse_users
+    url = user_url
+    response = RestClient.get(url)
+    data = JSON.parse(response)
+  end
+
+  def purchase_url
     url_extension = "https://driftrock-dev-test-2.herokuapp.com/purchases?per_page=40001&page=1"
     # total purchases = 40001
+  end
+
+  def user_url
+    url_extension = "https://driftrock-dev-test-2.herokuapp.com/users?per_page=10001&page=1"
+    # total users = 10001
   end
 
 end
--- spec/highest_value_spec.rb
+++ spec/highest_value_spec.rb
@@ -3,9 +3,16 @@ require 'spec_helper'
 describe MostLoyal do
 
   describe '#most_user_id' do
-    it 'returns the user id of the most loyal user' do
+    xit 'returns the user id of the most loyal user' do
       ml = MostLoyal.new
       expect(ml.mode_user_id).to eq ["NW0H-Q6RH-5L31-4ZHU", 15]
+    end
+  end
+
+  describe '#print users' do
+    it 'prints users' do
+      ml = MostLoyal.new
+      expect { ml.print_users }.to output("bogisich_judah@hilperttromp.biz\n").to_stdout
     end
   end
 
